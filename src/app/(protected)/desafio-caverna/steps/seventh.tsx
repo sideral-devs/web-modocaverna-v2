9b988cae5f4975a0d697293b636a94b9f745afbd
Artur_Linux <nevesdev.ti@gmail.com>
2025-04-07 17:55:10 -0300

    feat: ajustando video de apresentação do desafio

--- src/app/(protected)/desafio-caverna/steps/seventh.tsx
+++ src/app/(protected)/desafio-caverna/steps/seventh.tsx
@@ -1,61 +1,13 @@
-/* eslint-disable */
-import AutoSubmitButton from "@/components/ui/autoSubmitButton";
-import { api } from "@/lib/api";
-import { useChallengerStore } from "@/store/challenge";
-import { useQueryClient } from "@tanstack/react-query";
-import { AxiosError } from "axios";
-import { se } from "date-fns/locale";
-import { Loader2 } from "lucide-react";
-import Image from "next/image";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
-import { toast } from "sonner";
+import AutoSubmitButton from '@/components/ui/autoSubmitButton'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export function SeventhStep() {
-  const {
-    textarea_oque_motivou,
-    textarea_oque_deseja,
-    initialSituationPhotos,
-    initialReasonPhotos,
-    compromisses,
-    fail,
-  } = useChallengerStore();
-  const router = useRouter();
-  const queryClient = useQueryClient();
-  const [isLoading, setIsLoading] = useState(false);
-
-  async function handleActivate() {
-    try {
-      setIsLoading(true);
-      const response = await api.post("/desafios/store", {
-        textarea_oque_motivou,
-        textarea_oque_deseja,
-        fotos_situacao_inicial: initialSituationPhotos,
-        fotos_oque_motivou_inicial: initialReasonPhotos,
-        array_comprometimento: compromisses,
-        array_falhar: fail,
-        modalidade: "cavernoso_40",
-      });
-
-      localStorage.setItem("new-challenge-data", JSON.stringify(response.data));
-
-      localStorage.setItem("challenge-storage", "");
-
-      window.location.href = "/desafio-caverna/dashboard";
-    } catch (err) {
-      if (err instanceof AxiosError && err.response?.data) {
-        toast.error(err.response.data.message);
-      } else {
-        toast.error("Algo deu errado. Tente novamente.");
-      }
-    }
-  }
-
   return (
     <div className="flex flex-col relative flex-1 w-[140dvh] justify-between items-start">
       <div className="flex items-start pl-10 3xl:pb-8 pb-4 gap-16">
         <Image
-          src={"/images/lobo/apontando.png"}
+          src={'/images/lobo/apontando.png'}
           alt="Capitão Caverna"
           width={228}
           height={374}
@@ -84,7 +36,7 @@ export function SeventhStep() {
             </p>
           </div>
           <Image
-            src={"/images/triangle-balloon.svg"}
+            src={'/images/triangle-balloon.svg'}
             width={54}
             height={14}
             alt="balloon"
@@ -93,18 +45,10 @@ export function SeventhStep() {
         </div>
       </div>
       <footer className="flex w-full h-32 justify-center items-end  pb-11 gap-4">
-        <AutoSubmitButton
-          onClick={handleActivate}
-          disabled={isLoading}
-          className="min-w-[160px] flex justify-center items-center"
-        >
-          {isLoading ? (
-            <Loader2 className="animate-spin w-5 h-5" />
-          ) : (
-            <span>Entendido, Capitão!</span>
-          )}
-        </AutoSubmitButton>
+        <Link href={'/desafio-caverna/dashboard/tour'}>
+          <AutoSubmitButton>Entendido, Capitão!</AutoSubmitButton>
+        </Link>
       </footer>
     </div>
-  );
+  )
 }
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,6 +2,7 @@ export const videos = {
   mindfulness: '67980d2e921b5eb7e8739cea',
   caveRite: '678a9550d66c86f1df86d814',
   challengeTutorial: '67f0291bea0f253b2d1d4ef4',
+  challengeTourFinal: '67f3b59d902b8a612190ed20',
   onboardingTutorial: '67f0299ed75d2074cce0f8af',
   affiliateTutorial: '6769bf31572299dbc2d8df79',
 }
