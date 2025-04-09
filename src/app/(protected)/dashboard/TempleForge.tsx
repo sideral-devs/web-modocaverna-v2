ced1f8f199e874fce6fddda8b9c39cb847a7e4a8
Marcos Vinícius Pedreira Vieira <viniciuspv.si@gmail.com>
2025-04-09 14:54:56 -0300

    ajuste de imagem

--- src/app/(protected)/dashboard/TempleForge.tsx
+++ src/app/(protected)/dashboard/TempleForge.tsx
@@ -17,7 +17,8 @@ export function TempleForge({ value }: { value: string }) {
           <div
             className="absolute  right-1 bottom-5 w-full h-full"
             style={{
-              backgroundImage: "url('/images/card-frames/frame_esquerda.png')",
+              backgroundImage:
+                "url('/images/cursos-indique-e-ganhe/frame_esquerda.png')",
               backgroundPosition: 'center',
               backgroundRepeat: 'no-repeat',
               backgroundSize: 'FIT',
