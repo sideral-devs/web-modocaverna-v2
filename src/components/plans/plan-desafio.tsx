3f0388ffbf22bed7ec7e2662fabea9581b159c06
Artur_Linux <nevesdev.ti@gmail.com>
2025-04-11 16:26:08 -0300

    feat: onboarding desafio modal

--- src/components/plans/plan-desafio.tsx
+++ src/components/plans/plan-desafio.tsx
@@ -1,11 +1,23 @@
-import { Check, X } from 'lucide-react'
-
-export function PlanDesafio() {
+//  import dayjs from 'dayjs'
+import { Check } from 'lucide-react'
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function PlanDesafio({ user }: { user: User }): JSX.Element {
+  const listToolsDesafio = [
+    'Curso Modo Caverna',
+    'Desafio Caverna',
+    'Indique e ganhe',
+    'Suporte via chat',
+  ]
   return (
-    <div className="flex  w-80 bg-gradient-to-b from-[#2a2a2a] from-[12%] to-zinc-900 rounded-2xl">
-      <div className="flex flex-col py-8 gap-6 w-full">
-        <div className="flex items-center justify-center">
-          <h3 className=""> Plano Desafio Caverna </h3>
+    <div className="flex h-[455px] w-[337px] border border-zinc-700 rounded-2xl">
+      <div className="flex flex-col pt-10 gap-6 w-full">
+        <div className="flex flex-col gap-8 pb-4 px-5 items-start justify-center">
+          <h4 className="text-yellow-300 text-[14px]">Seu plano atual</h4>
+          <h3 className=" text-xl"> Plano Caverna </h3>
+          {/* <p className="text-xs">
+              Assinatura garantida até{" "}
+              <strong className="text-red-700">{dayjs(user.data_de_renovacao).format('DD/MM/YYYY')}</strong>
+            </p> */}
 
           {/* <div className="flex items-end text-emerald-400 gap-2">
           <h3 className="text-5xl">
@@ -15,72 +27,17 @@ export function PlanDesafio() {
           <span>/ {selectedPlan === 'yearly' ? 'ano' : 'mês'}</span>
         </div> */}
         </div>
-        <div className="w-full h-[1px] bg-zinc-700" />
-        <div className="flex flex-col pb-5 px-6 gap-6">
-          <span className="flex w-fit px-3 py-1 text-xs font-semibold bg-red-700  rounded-full">
-            6 meses de Acesso
-          </span>
+
+        <div className="flex flex-col h-full bg-zinc-800 py-5 px-6 rounded-2xl gap-6">
+          <h5 className="text-sm text-zinc-400">Você tem acesso a:</h5>
           <ul className="flex flex-col gap-5">
-            <li className="flex items-center gap-4">
-              <Check size={16} className="text-emerald-400" />
-              <span className="text-xs text-zinc-400">
-                Desafio Modo caverna
-              </span>
-            </li>
-            <li className="flex items-center gap-4">
-              <X size={16} className="text-primary" />
-              <span className="text-xs line-through text-zinc-400">
-                Flow Produtividade
-              </span>
-            </li>
-            <li className="flex items-center gap-4">
-              <X size={16} className="text-primary" />
-              <span className="text-xs line-through text-zinc-400">
-                Gestão de Metas Anuais
-              </span>
-            </li>
-            <li className="flex items-center gap-4">
-              <X size={16} className="text-primary" />
-              <span className="text-xs line-through text-zinc-400">
-                Agenda Integrada ao Google
-              </span>
-            </li>
-            <li className="flex items-center gap-4">
-              <X size={16} className="text-primary" />
-              <span className="text-xs line-through text-zinc-400">
-                Gestão Financeira Pessoal
-              </span>
-            </li>
-            <li className="flex items-center gap-4">
-              <X size={16} className="text-primary" />
-              <span className="text-xs line-through text-zinc-400">
-                Assistente Pessoal com IA
-              </span>
-            </li>
-            <li className="flex items-center gap-4">
-              <X size={16} className="text-primary" />
-              <span className="text-xs line-through text-zinc-400">
-                Organização de Biblioteca
-              </span>
-            </li>
-            <li className="flex items-center gap-4">
-              <X size={16} className="text-primary" />
-              <span className="text-xs line-through text-zinc-400">
-                Acesso a Cursos e Vídeos
-              </span>
-            </li>
-            <li className="flex items-center gap-4">
-              <X size={16} className="text-primary" />
-              <span className="text-xs line-through text-zinc-400">
-                Conteúdos Exclusivos
-              </span>
-            </li>
-            <li className="flex items-center gap-4">
-              <X size={16} className="text-primary" />
-              <span className="text-xs line-through text-zinc-400">
-                Suporte Humanizado via Chat
-              </span>
-            </li>
+            {listToolsDesafio &&
+              listToolsDesafio.map((tool, index) => (
+                <li key={index} className="flex items-center gap-4">
+                  <Check size={16} className="text-amber-400" />
+                  <span className="text-sm">{tool}</span>
+                </li>
+              ))}
           </ul>
           {/* <Link
           href={getPlanUrl()}
