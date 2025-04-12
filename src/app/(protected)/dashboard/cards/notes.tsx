4685525f269cf824fbd4e2296d376c99b0e9aade
Marcos Vinícius Pedreira Vieira <viniciuspv.si@gmail.com>
2025-04-12 18:48:19 -0300

    ajustes de CTAS

--- src/app/(protected)/dashboard/cards/notes.tsx
+++ src/app/(protected)/dashboard/cards/notes.tsx
@@ -140,7 +140,7 @@ export default function NotesCard() {
       </div>
       <Link className="ml-auto" href="/anotacoes">
         <Button className="block" size="sm">
-          Abrir Anotações
+          Abrir Notas
         </Button>
       </Link>
     </Card>
