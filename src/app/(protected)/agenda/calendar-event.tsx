865adaa821bc1fe67ac885819e43f3b0615750bf
john0x01 <personal.joaomarcelo.rocha@gmail.com>
2025-04-15 19:46:48 -0300

    refactor: Calendário

--- src/app/(protected)/agenda/calendar-event.tsx
+++ src/app/(protected)/agenda/calendar-event.tsx
@@ -1,9 +1,11 @@
 'use client'
 import { cn } from '@/lib/utils'
+import { parse } from 'date-fns'
 import dayjs from 'dayjs'
 import 'dayjs/locale/pt-br'
 import customParseFormat from 'dayjs/plugin/customParseFormat'
 import { EditEventDialogTrigger } from './edit-event'
+import { GoogleEditEventDialogTrigger } from './google-edit-event'
 
 dayjs.locale('pt-br')
 dayjs.extend(customParseFormat)
@@ -11,30 +13,133 @@ dayjs.extend(customParseFormat)
 // Utilizar para cores customizadas por categoria
 const eventColors = {
   google: {
-    bg: '#713f12',
     text: '#facc15',
   },
   default: {
-    bg: '#27272a',
     text: '#3b82f6',
   },
   Compromisso: {
-    bg: '#27272a',
     text: '#3b82f6',
   },
   Refeição: {
-    bg: '#27272a',
     text: '#facc15',
   },
   Treino: {
-    bg: '#27272a',
     text: '#ef4444',
   },
   Pessoal: {
-    bg: '#27272a',
-    text: '#22C45E',
+    text: '#22c55e',
   },
-} as { [key: string]: { bg: string; text: string } }
+  Ritual: {
+    text: '#facc15',
+  },
+} as { [key: string]: { text: string } }
+
+function BaseEvent({
+  start,
+  end,
+  allDay,
+  title,
+  category,
+  checked,
+  mode = 'weekly',
+}: {
+  start: string
+  end: string
+  title: string
+  category: string
+  allDay?: boolean
+  checked: boolean
+  mode?: 'weekly' | 'daily'
+}) {
+  const PIXELS_PER_MINUTE = 112 / 60
+  const DAY_WIDTH = 160
+
+  const minHeight = 44
+
+  const eventStart = dayjs(start, 'YYYY-MM-DD HH:mm')
+  const eventEnd = dayjs(end, 'YYYY-MM-DD HH:mm')
+  const dayIndex = eventStart.day()
+
+  const startMinutes = eventStart.hour() * 60 + eventStart.minute()
+  const endMinutes = eventEnd.hour() * 60 + eventEnd.minute()
+
+  const top = startMinutes * PIXELS_PER_MINUTE
+  const height = Math.max(
+    (endMinutes - startMinutes) * PIXELS_PER_MINUTE,
+    minHeight,
+  )
+
+  let isAllDay = false
+  let dayCount = 0
+
+  if (!eventStart.isSame(eventEnd, 'day')) {
+    if (!allDay) {
+      return null
+    }
+    isAllDay = true
+    dayCount = eventEnd.diff(eventStart, 'days')
+  }
+
+  return (
+    <div
+      className={cn(
+        'flex flex-col items-start py-3 px-4 gap-2 absolute text-white rounded-lg z-10 overflow-y-hidden cursor-pointer',
+        'border-l-4 shadow-md overflow-hidden pointer-events-auto',
+        category === 'google' || category === 'Ritual'
+          ? checked
+            ? 'bg-yellow-950'
+            : 'bg-yellow-900'
+          : 'bg-zinc-800',
+        isAllDay && '2xl:translate-x-4',
+      )}
+      style={{
+        borderColor: eventColors[category].text,
+        top: isAllDay ? 64 : `${top}px`,
+        height: isAllDay ? 44 : `${height}px`,
+        left: isAllDay
+          ? 65 + 4 + DAY_WIDTH * dayIndex
+          : mode === 'daily'
+            ? 80
+            : 4,
+        ...(() => (isAllDay ? {} : { right: 4 }))(),
+        ...(() =>
+          isAllDay
+            ? {
+                width: (() => {
+                  const maxLength = (7 - dayIndex) * DAY_WIDTH - 8
+                  const minLength = DAY_WIDTH - 8
+
+                  return Math.min(
+                    Math.max((dayCount + 1) * DAY_WIDTH - 8, minLength),
+                    maxLength,
+                  )
+                })(),
+              }
+            : {})(), // Para não ter essa propriedade para eventos do dia todo
+        maxWidth: '100%',
+      }}
+    >
+      <span
+        className={`${height < 80 ? 'text-[10px]' : 'text-xs'} font-medium`}
+        style={{
+          color: eventColors[category].text,
+        }}
+      >
+        {`${eventStart.format('HH:mm')} - ${eventEnd.format('HH:mm')}`}
+      </span>
+      <p
+        className={cn(
+          'line-clamp-2',
+          checked && 'line-through opacity-75',
+          height < 80 ? 'text-[10px]' : 'text-xs',
+        )}
+      >
+        {title}
+      </p>
+    </div>
+  )
+}
 
 export function CalendarEvent({
   event,
@@ -47,14 +152,8 @@ export function CalendarEvent({
   mode: 'weekly' | 'daily'
   allDay?: boolean
 }) {
-  const PIXELS_PER_MINUTE = 112 / 60
-  const minHeight = 44
-
-  const DAY_WIDTH = 160
-
   const eventStart = dayjs(event.comeca, 'YYYY-MM-DD HH:mm')
   const eventEnd = dayjs(event.termina, 'YYYY-MM-DD HH:mm')
-  const dayIndex = eventStart.day()
 
   if (mode === 'weekly') {
     if (
@@ -72,119 +171,98 @@ export function CalendarEvent({
     }
   }
 
-  const startMinutes = eventStart.hour() * 60 + eventStart.minute()
-  const endMinutes = eventEnd.hour() * 60 + eventEnd.minute()
-
-  const top = startMinutes * PIXELS_PER_MINUTE
-  const height = Math.max(
-    (endMinutes - startMinutes) * PIXELS_PER_MINUTE,
-    minHeight,
-  )
-
-  const diff = endMinutes - startMinutes
-  const size = diff >= 60 ? 'lg' : diff >= 30 ? 'md' : 'sm'
-
-  const color = eventColors[event.categoria]
-  let isAllDay = false
-  let dayCount = 0
-
-  if (!eventStart.isSame(eventEnd, 'day')) {
-    if (!allDay) {
-      return null
-    }
-    isAllDay = true
-    dayCount = eventEnd.diff(eventStart, 'days')
-  }
-
-  // console.log({
-  //   isAllDay,
-  //   DAY_WIDTH,
-  //   dayIndex,
-  //   top: isAllDay ? 64 : `${top}px`,
-  //   height: isAllDay ? 44 : `${height}px`,
-  //   left: isAllDay ? 65 + 4 + DAY_WIDTH * dayIndex : 4,
-  //   ...(() => (isAllDay ? {} : { right: 4 }))(),
-  // })
-
   return (
     <EditEventDialogTrigger event={event}>
-      <div
-        className={cn(
-          'flex py-3 px-4 absolute text-white rounded-lg z-10 overflow-y-hidden cursor-pointer',
-          size === 'md' ? 'flex-row items-center gap-1' : 'flex-col',
-          isAllDay && '2xl:translate-x-4',
-        )}
-        style={{
-          top: isAllDay ? 64 : `${top}px`,
-          height: isAllDay ? 44 : `${height}px`,
-          // left: isAllDay ? base 65 + 4 + DAY_WIDTH * dayIndex : 4,
-          left: mode === 'weekly' ? 65 + 4 + DAY_WIDTH * dayIndex : 69,
-          ...(() => (isAllDay ? {} : { right: 4 }))(),
-          backgroundColor: color?.bg || eventColors.default.bg,
-          ...(() =>
-            isAllDay
-              ? {
-                  width: (() => {
-                    const maxLength = (7 - dayIndex) * DAY_WIDTH - 8
-                    const minLength = DAY_WIDTH - 8
-
-                    return Math.min(
-                      Math.max((dayCount + 1) * DAY_WIDTH - 8, minLength),
-                      maxLength,
-                    )
-                  })(),
-                }
-              : {})(), // Para não ter essa propriedade para eventos do dia todo
-          maxWidth: '100%',
-        }}
-      >
-        {size === 'lg' && !isAllDay && (
-          <span
-            className={`flex w-fit py-1 px-2 rounded-full bg-white text-zinc-500 text-[0.5rem] uppercase mt-3`}
-          >
-            {event.categoria}
-          </span>
-        )}
-        <p
-          className={cn(
-            'text-xs line-clamp-2',
-            event.checked && 'line-through opacity-75',
-          )}
-          style={{
-            marginTop: isAllDay
-              ? 0
-              : size === 'lg'
-                ? 12
-                : size === 'sm'
-                  ? 4
-                  : 0,
-          }}
-        >
-          {event.titulo}
-        </p>
-        {!isAllDay && (
-          <span
-            style={{
-              fontSize: size === 'lg' ? 12 : 10,
-              marginLeft: size === 'md' ? 'auto' : 0,
-              marginTop: size === 'lg' ? 8 : 0,
-              color: color?.text || eventColors.default.text,
-            }}
-          >
-            {`${eventStart.hour()}h${eventStart.minute().toString().padEnd(2, '0')}`}
-            {' - '}
-            {`${eventEnd.hour()}h${eventEnd.minute().toString().padEnd(2, '0')}`}
-          </span>
-        )}
-        {event.categoria.toLowerCase() !== 'google' && (
-          <div
-            className="absolute left-1 top-2 bottom-2 w-[3px] rounded-full"
-            style={{
-              backgroundColor: color?.text || eventColors.default.text,
-            }}
-          />
-        )}
-      </div>
+      <button className="block">
+        <BaseEvent
+          start={event.comeca}
+          end={event.termina}
+          title={event.titulo}
+          category={event.categoria}
+          checked={event.checked}
+          allDay={allDay}
+          mode={mode}
+        />
+      </button>
     </EditEventDialogTrigger>
+  )
+}
+
+export function GoogleEvent({
+  event,
+  now,
+  mode,
+  allDay = false,
+}: {
+  event: GoogleEvent & { compromisso_id: string; categoria: string }
+  now: Date | undefined
+  mode: 'weekly' | 'daily'
+  allDay?: boolean
+}) {
+  const eventStart = dayjs(event.comeca, 'YYYY-MM-DD HH:mm')
+  const eventEnd = dayjs(event.termina, 'YYYY-MM-DD HH:mm')
+
+  if (mode === 'weekly') {
+    if (
+      eventEnd.isBefore(dayjs(now).startOf('week')) ||
+      eventStart.isAfter(dayjs(now).endOf('week'))
+    ) {
+      return null
+    }
+  } else {
+    if (
+      eventEnd.isBefore(dayjs(now).startOf('day')) ||
+      eventStart.isAfter(dayjs(now).endOf('day'))
+    ) {
+      return null
+    }
+  }
+
+  return (
+    <GoogleEditEventDialogTrigger event={event}>
+      <button className="block">
+        <BaseEvent
+          start={event.comeca}
+          end={event.termina}
+          title={event.titulo}
+          category={event.categoria}
+          checked={event.checked}
+          allDay={allDay}
+          mode={mode}
+        />
+      </button>
+    </GoogleEditEventDialogTrigger>
+  )
+}
+
+export function RitualEvent({
+  title,
+  timeStart,
+  timeEnd,
+  mode,
+}: {
+  title: string
+  timeStart: string
+  timeEnd: string
+  now: Date | undefined
+  mode: 'weekly' | 'daily'
+}) {
+  const start = dayjs(parse(timeStart, 'HH:mm', new Date())).format(
+    'YYYY-MM-DD HH:mm',
+  )
+  const end = dayjs(parse(timeEnd, 'HH:mm', new Date())).format(
+    'YYYY-MM-DD HH:mm',
+  )
+
+  return (
+    <BaseEvent
+      start={start}
+      end={end}
+      title={title}
+      category="Ritual"
+      checked={false}
+      allDay={false}
+      mode={mode}
+    />
   )
 }
--- src/app/(protected)/agenda/calendar.tsx
+++ src/app/(protected)/agenda/calendar.tsx
@@ -3,15 +3,13 @@ import { Calendar } from '@/components/ui/calendar'
 import { api } from '@/lib/api'
 import { cn } from '@/lib/utils'
 import { useQuery } from '@tanstack/react-query'
-import { parse } from 'date-fns'
 import { ptBR } from 'date-fns/locale'
 import dayjs from 'dayjs'
 import 'dayjs/locale/pt-br'
 import customParseFormat from 'dayjs/plugin/customParseFormat'
 import { useSession } from 'next-auth/react'
 import { useEffect, useRef, useState } from 'react'
-import { EditEventDialogTrigger } from './edit-event'
-import { GoogleEditEventDialogTrigger } from './google-edit-event'
+import { CalendarEvent, GoogleEvent, RitualEvent } from './calendar-event'
 
 dayjs.locale('pt-br')
 dayjs.extend(customParseFormat)
@@ -94,8 +92,7 @@ export function EventCalendar() {
 
   return (
     <section className="flex w-full max-w-[1512px] h-[calc(100vh-98px)]">
-      {/* Sidebar Calendar */}
-      <div className="flex flex-col max-w-72 w-72 border-r border-zinc-800 overflow-y-auto scrollbar-minimal bg-zinc-950">
+      <div className="hidden lg:flex flex-col w-72 min-w-72 border-r overflow-y-auto scrollbar-minimal">
         <Calendar
           mode="single"
           selected={selected}
@@ -104,100 +101,85 @@ export function EventCalendar() {
           locale={ptBR}
         />
       </div>
-
-      {/* Main Calendar Grid */}
-      <div
-        ref={scrollableRef}
-        className="flex flex-col flex-1 h-full overflow-y-auto scrollbar-minimal bg-zinc-950"
-      >
-        {/* Weekday Header */}
-        <div className="flex w-full min-w-fit pl-16 bg-zinc-900 border-b border-zinc-800 sticky top-0 z-20">
-          {WEEKDAYS.map((day, index) => (
-            <div
-              key={index}
-              className="flex flex-col w-full min-w-36 h-14 p-1 items-center justify-end"
-            >
-              <span className="text-zinc-500 text-xs capitalize">{day}</span>
-              <span
-                className={cn(
-                  'text-white text-sm capitalize p-1 rounded-full',
-                  dayjs(selected)
+      <div className="flew h-[calc(100vh-98px)] flex-1 pb-4 overflow-hidden">
+        <div
+          ref={scrollableRef}
+          className="flex flex-col xl:w-full h-full flex-1 overflow-auto scrollbar-minimal bg-black relative"
+        >
+          <div className="flex w-full min-w-fit pl-16 bg-zinc-900 border-b sticky top-0 left-0 z-50">
+            {WEEKDAYS.map((day, index) => (
+              <div
+                key={index}
+                className="flex flex-col min-w-40 w-40 xl:w-full h-14 p-1 items-center justify-end"
+              >
+                <span className="text-zinc-500 text-xs capitalize">{day}</span>
+                <span
+                  className={cn(
+                    'text-white text-sm capitalize p-1 rounded-full',
+                    dayjs(selected)
+                      .startOf('week')
+                      .add(index, 'day')
+                      .isSame(dayjs().startOf('day')) && 'bg-primary',
+                  )}
+                >
+                  {dayjs(selected)
                     .startOf('week')
                     .add(index, 'day')
-                    .isSame(dayjs().startOf('day')) && 'bg-primary',
-                )}
-              >
-                {dayjs(selected).startOf('week').add(index, 'day').format('DD')}
-              </span>
-            </div>
-          ))}
-        </div>
-
-        {/* Time Grid */}
-        <div className="flex-1 min-h-0 relative">
-          <div className="flex flex-col h-full max-h-[calc(100vh-100px)]">
-            {Array.from({ length: 24 }).map((_, index) => (
-              <div key={index} className="flex items-center">
-                <div className="flex w-16 min-w-16 h-28 items-end justify-end p-2 bg-zinc-900 border-r border-zinc-800">
+                    .format('DD')}
+                </span>
+              </div>
+            ))}
+          </div>
+          <div className="flex flex-1 min-h-0 h-full max-h-[calc(100vh-100px)] relative">
+            <div className="flex flex-col bg-zinc-900 border-r">
+              {Array.from({ length: 24 }).map((_, index) => (
+                <div
+                  key={index}
+                  className="flex w-16 min-w-16 min-h-28 items-end justify-end p-2 bg-zinc-900 border-r"
+                >
                   <span className="text-sm text-zinc-500">
                     {(index < 23 ? index + 1 : 0).toFixed(0).padStart(2, '0')}
                     :00
                   </span>
                 </div>
-                <div className="flex w-full h-28 border-b border-zinc-800">
-                  {Array.from({ length: 7 }).map((_, i) => (
-                    <div
-                      key={i}
-                      className="min-w-36 w-full h-full border-r border-zinc-800"
-                    ></div>
-                  ))}
-                </div>
-              </div>
-            ))}
-
-            {/* Events */}
-            <div className="absolute inset-0 pointer-events-none">
-              {compromissos?.compromissos.map((event) => (
-                <CompromissoEvent
-                  event={event}
-                  key={event.compromisso_id}
-                  now={selected}
-                />
               ))}
-              {googleEvents &&
-                (googleEvents?.events ?? []).map((event) => (
-                  <GoogleEvent
-                    event={{
-                      ...event,
-                      compromisso_id: event.event_id,
-                      repete: event.repete,
-                      categoria: 'google',
-                    }}
-                    key={event.event_id}
-                    now={selected}
+            </div>
+            <NowIndicator />
+            <div className="flex w-full flex-1 h-fit">
+              {Array.from({ length: 7 }).map((_, i) => {
+                const columnDate = dayjs(startWeek).add(i, 'day')
+
+                const eventsNormal = compromissos
+                  ? compromissos.compromissos.filter((event) =>
+                      dayjs(event.comeca).isSame(columnDate, 'day'),
+                    )
+                  : []
+
+                const googleCalendarEvents =
+                  googleEvents && googleEvents.events
+                    ? googleEvents.events
+                        .filter((event) =>
+                          dayjs(event.comeca).isSame(columnDate, 'day'),
+                        )
+                        .map((e) => ({
+                          ...e,
+                          compromisso_id: e.event_id,
+                          repete: e.repete,
+                          categoria: 'google' as const,
+                        }))
+                    : []
+
+                return (
+                  <Column
+                    key={i}
+                    date={columnDate}
+                    events={eventsNormal}
+                    googleEvents={googleCalendarEvents}
+                    morningRitual={morningRitual}
+                    nightRitual={nightRitual}
                   />
-                ))}
-              {morningRitual &&
-                Array.from({ length: 7 }).map((i, index) => (
-                  <RitualEvent
-                    key={index}
-                    title="Ritual Matinal"
-                    dayIndex={index}
-                    timeStart={morningRitual.horario_inicial}
-                    timeEnd={morningRitual.horario_final}
-                  />
-                ))}
-              {nightRitual &&
-                Array.from({ length: 7 }).map((i, index) => (
-                  <RitualEvent
-                    key={index}
-                    title="Ritual Noturno"
-                    dayIndex={index}
-                    timeStart={nightRitual.horario_inicial}
-                    timeEnd={nightRitual.horario_final}
-                  />
-                ))}
-              <NowIndicator />
+                )
+              })}
             </div>
           </div>
         </div>
@@ -206,221 +188,60 @@ export function EventCalendar() {
   )
 }
 
-function CompromissoEvent({
-  event,
-  now,
+function Column({
+  date,
+  events,
+  googleEvents,
+  morningRitual,
+  nightRitual,
 }: {
-  event: Compromisso
-  now: Date | undefined
+  date: dayjs.Dayjs
+  events: Compromisso[]
+  googleEvents: Array<
+    GoogleEvent & { compromisso_id: string; categoria: string }
+  >
+  morningRitual?: RitualResponseItem
+  nightRitual?: RitualResponseItem
 }) {
-  const PIXELS_PER_MINUTE = 112 / 60
-  const minHeight = 44
-
-  const eventStart = dayjs(event.comeca, 'YYYY-MM-DD HH:mm')
-  const eventEnd = dayjs(event.termina, 'YYYY-MM-DD HH:mm')
-
-  const dayIndex = eventStart.day()
-
-  if (
-    eventEnd.isBefore(dayjs(now).startOf('week')) ||
-    eventStart.isAfter(dayjs(now).endOf('week'))
-  ) {
-    return null
-  }
-
-  const startMinutes = eventStart.hour() * 60 + eventStart.minute()
-  const endMinutes = eventEnd.hour() * 60 + eventEnd.minute()
-
-  const top = startMinutes * PIXELS_PER_MINUTE
-  const height = Math.max(
-    (endMinutes - startMinutes) * PIXELS_PER_MINUTE,
-    minHeight,
-  )
-
-  const baseLeft = 70
-
   return (
-    <EditEventDialogTrigger event={event}>
-      <div
-        className={`
-          absolute flex flex-col py-2 px-4 gap-2 w-32 2xl:w-36 3xl:w-40 
-          bg-zinc-800 hover:bg-zinc-700 text-white rounded-lg 
-          border-l-4 ${
-            event.categoria === 'Compromisso'
-              ? ' border-blue-500'
-              : event.categoria === 'Refeição'
-                ? ' border-yellow-500'
-                : event.categoria === 'Treino'
-                  ? ' border-red-500'
-                  : event.categoria === 'Pessoal'
-                    ? ' border-green-500'
-                    : ' border-zinc-500'
-          } cursor-pointer transition-colors
-          shadow-md overflow-hidden pointer-events-auto
-        `}
-        style={{
-          top: `${top}px`,
-          height: `${height}px`,
-          left: `calc(${baseLeft}px + ${dayIndex * 144}px)`,
-        }}
-      >
-        <span
-          className={`${height < 80 ? 'text-[10px]' : 'text-xs'} font-medium  ${
-            event.categoria === 'Compromisso'
-              ? ' text-blue-400'
-              : event.categoria === 'Refeição'
-                ? ' text-yellow-400'
-                : event.categoria === 'Treino'
-                  ? ' text-red-400'
-                  : event.categoria === 'Pessoal'
-                    ? ' text-green-400'
-                    : ' text-zinc-400'
-          }`}
-        >
-          {`${eventStart.format('HH:mm')} - ${eventEnd.format('HH:mm')}`}
-        </span>
-        <p
-          className={`${height < 80 ? 'text-[10px]' : 'text-xs'} font-medium line-clamp-2`}
-        >
-          {event.titulo}
-        </p>
-
-        {/* {event.descricao && (
-          <p className="text-xs text-zinc-400 line-clamp-2">
-            {event.descricao}
-          </p>
-        )} */}
-      </div>
-    </EditEventDialogTrigger>
-  )
-}
-
-function GoogleEvent({
-  event,
-  now,
-}: {
-  event: GoogleEvent & { compromisso_id: string; categoria: string }
-  now: Date | undefined
-}) {
-  const PIXELS_PER_MINUTE = 112 / 60
-  const minHeight = 44
-
-  const eventStart = dayjs(event.comeca, 'YYYY-MM-DD HH:mm')
-  const eventEnd = dayjs(event.termina, 'YYYY-MM-DD HH:mm')
-
-  const dayIndex = eventStart.day()
-
-  if (
-    eventEnd.isBefore(dayjs(now).startOf('week')) ||
-    eventStart.isAfter(dayjs(now).endOf('week'))
-  ) {
-    return null
-  }
-
-  const startMinutes = eventStart.hour() * 60 + eventStart.minute()
-  const endMinutes = eventEnd.hour() * 60 + eventEnd.minute()
-
-  const top = startMinutes * PIXELS_PER_MINUTE
-  const height = Math.max(
-    (endMinutes - startMinutes) * PIXELS_PER_MINUTE,
-    minHeight,
-  )
-
-  const baseLeft = 80
-
-  return (
-    <GoogleEditEventDialogTrigger event={event}>
-      <div
-        className={`
-          absolute flex flex-col py-3 px-4 gap-2 w-32 2xl:w-36 3xl:w-40 
-          ${event.checked ? 'bg-yellow-950' : 'bg-yellow-900'}
-          hover:bg-yellow-800 text-white rounded-lg 
-          border-l-4 border-yellow-500 cursor-pointer transition-colors
-          shadow-md overflow-hidden pointer-events-auto
-        `}
-        style={{
-          top: `${top}px`,
-          height: `${height}px`,
-          left: `calc(${baseLeft}px + ${dayIndex * 144}px)`,
-        }}
-      >
-        <span
-          className={`${height < 80 ? 'text-[10px]' : 'text-xs'} font-medium text-yellow-400`}
-        >
-          {`${eventStart.format('HH:mm')} - ${eventEnd.format('HH:mm')}`}
-        </span>
-        {height >= 80 && (
-          <p
-            className={`${height < 80 ? 'text-[10px]' : 'text-xs'} font-normal line-clamp-2 ${event.checked ? 'line-through opacity-75' : ''}`}
-          >
-            {event.titulo}
-          </p>
-        )}
-        {/* {event.descricao && (
-          <p className="text-xs text-yellow-200 line-clamp-2">
-            {event.descricao}
-          </p>
-        )} */}
-      </div>
-    </GoogleEditEventDialogTrigger>
-  )
-}
-
-function RitualEvent({
-  title,
-  timeStart,
-  timeEnd,
-  dayIndex,
-}: {
-  title: string
-  timeStart: string
-  timeEnd: string
-  dayIndex: number
-}) {
-  const PIXELS_PER_MINUTE = 112 / 60
-  const minHeight = 44
-
-  const start = parse(timeStart, 'HH:mm', new Date())
-  const end = parse(timeEnd, 'HH:mm', new Date())
-
-  const startMinutes = start.getHours() * 60 + start.getMinutes()
-  const endMinutes = end.getHours() * 60 + end.getMinutes()
-
-  const top = startMinutes * PIXELS_PER_MINUTE
-  const height = Math.max(
-    (endMinutes - startMinutes) * PIXELS_PER_MINUTE,
-    minHeight,
-  )
-
-  const baseLeft = 80
-
-  return (
-    <div
-      className={`
-          absolute flex flex-col py-3 px-4 gap-2 w-32 2xl:w-36 3xl:w-40
-          bg-yellow-900 hover:bg-yellow-800 text-white rounded-lg 
-          border-l-4 border-yellow-500 cursor-pointer transition-colors
-          shadow-md overflow-hidden pointer-events-auto
-        `}
-      style={{
-        top: `${top}px`,
-        height: `${height}px`,
-        left: `calc(${baseLeft}px + ${dayIndex * 144}px)`,
-      }}
-    >
-      <span
-        className={`${height < 80 ? 'text-[10px]' : 'text-xs'} font-medium text-yellow-400`}
-      >
-        {timeStart + ' - ' + timeEnd}
-      </span>
-
-      <p className={`text-xs font-normal line-clamp-2`}>{title}</p>
-
-      {/* {event.descricao && (
-          <p className="text-xs text-yellow-200 line-clamp-2">
-            {event.descricao}
-          </p>
-        )} */}
+    <div className="xl:w-full min-w-40 w-40 h-full flex-1 border-r relative">
+      {Array.from({ length: 24 }).map((_, index) => (
+        <div key={index} className="flex min-w-40 w-40 h-28 border-b"></div>
+      ))}
+      {events.map((event) => (
+        <CalendarEvent
+          event={event}
+          key={event.compromisso_id}
+          now={date.toDate()}
+          mode="weekly"
+        />
+      ))}
+      {googleEvents.map((event) => (
+        <GoogleEvent
+          event={event}
+          key={event.compromisso_id}
+          now={date.toDate()}
+          mode="weekly"
+        />
+      ))}
+      {morningRitual && (
+        <RitualEvent
+          mode="weekly"
+          now={date.toDate()}
+          timeStart={morningRitual.horario_inicial}
+          timeEnd={morningRitual.horario_final}
+          title="Ritual Matinal"
+        />
+      )}
+      {nightRitual && (
+        <RitualEvent
+          mode="weekly"
+          now={date.toDate()}
+          timeStart={nightRitual.horario_inicial}
+          timeEnd={nightRitual.horario_final}
+          title="Ritual Noturno"
+        />
+      )}
     </div>
   )
 }
