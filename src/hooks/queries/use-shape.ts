833d5c6c1b9aff1ecf86b47d4933e0b0f09a24e1
Eder Rosa <eder_junior07@hotmail.com>
2025-04-18 23:43:02 -0300

    feat(ficha de treinos: create, update)

--- src/hooks/queries/use-shape.ts
+++ src/hooks/queries/use-shape.ts
@@ -1,7 +1,15 @@
-import { useQuery } from '@tanstack/react-query'
-import { getShapeRegistrations } from '@/lib/api/shape'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import {
+  getShapeRegistrations,
+  createShapeRegistration,
+  updateShapeRegistration,
+  deleteShapeRegistration,
+} from '@/lib/api/shape'
+import { ShapeRegistration } from '@/app/(protected)/exercicios/page'
 
 export function useShape() {
+  const queryClient = useQueryClient()
+
   const {
     data: shapeRegistrations,
     isLoading,
@@ -11,12 +19,40 @@ export function useShape() {
     queryFn: getShapeRegistrations,
   })
 
-  const hasRegistration = Boolean(shapeRegistrations?.length)
+  const createShapeRegistrationMutation = useMutation({
+    mutationFn: createShapeRegistration,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['shape-registrations'] })
+    },
+  })
+
+  const updateShapeRegistrationMutation = useMutation({
+    mutationFn: ({
+      id,
+      data,
+    }: {
+      id: number
+      data: Partial<ShapeRegistration>
+    }) => updateShapeRegistration(id.toString(), data),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['shape-registrations'] })
+    },
+  })
+
+  const deleteShapeRegistrationMutation = useMutation({
+    mutationFn: deleteShapeRegistration,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['shape-registrations'] })
+    },
+  })
 
   return {
     shapeRegistrations,
     isLoading,
     error,
-    hasRegistration,
+    hasRegistration: shapeRegistrations && shapeRegistrations.length > 0,
+    createShapeRegistration: createShapeRegistrationMutation.mutate,
+    updateShapeRegistration: updateShapeRegistrationMutation.mutate,
+    deleteShapeRegistration: deleteShapeRegistrationMutation.mutate,
   }
 }
