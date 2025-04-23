1efabd8bd8e122c8ae5e17891d201fcd88791c36
john0x01 <personal.joaomarcelo.rocha@gmail.com>
2025-04-22 21:57:18 -0300

    fix: Linguagem do calendário

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { ptBR } from 'date-fns/locale'
 import { ChevronLeft, ChevronRight } from 'lucide-react'
 import * as React from 'react'
 import { DayPicker } from 'react-day-picker'
@@ -23,7 +24,7 @@ function Calendar({
         months: 'flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0',
         month: 'space-y-4',
         caption: 'flex justify-center pt-1 relative items-center',
-        caption_label: 'text-sm font-medium',
+        caption_label: 'text-sm font-medium capitalize',
         nav: 'space-x-1 flex items-center',
         nav_button: cn(
           buttonVariants({ variant: 'outline' }),
@@ -61,6 +62,7 @@ function Calendar({
           <ChevronRight className={cn('h-4 w-4', className)} {...props} />
         ),
       }}
+      locale={ptBR}
       {...props}
     />
   )
