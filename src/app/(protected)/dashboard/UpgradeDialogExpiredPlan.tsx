df62901c357ab0bad54c62602556b3fa0665e7ce
Marcos Vinícius Pedreira Vieira <viniciuspv.si@gmail.com>
2025-04-23 14:52:16 -0300

    adicionando modais

--- src/app/(protected)/dashboard/UpgradeDialogExpiredPlan.tsx
+++ src/app/(protected)/dashboard/UpgradeDialogExpiredPlan.tsx
@@ -28,35 +28,44 @@ export function UpgradeDialogExpiredPlan({
             alt="Imagens modo caverna"
           />
         </div>
-        <div className="flex flex-col w-[490px] h-[500px] pt-7">
+        <div className="flex flex-col w-[490px] h-[500px] pt-4">
           <div className="flex flex-row ">
             <AlertTitle className="text-2xl font-semibold text-white">
-              Parece que a sua avaliação gratuita de 7 dias ao Plano
-              Cavernoso&nbsp;
-              <span className="text-primary">expirou...</span>
+              Fala Caverna, sinto sua falta por aqui...
             </AlertTitle>
           </div>
           <p className="text-base pt-4 text-[#A0A0A9]">
-            O seu acesso ao Modo Caverna ainda está garantido, mas a sua
-            assinatura atual contempla apenas algumas funcionalidades...
+            Sua assinatura expirou, e por isso o acesso às ferramentas do seu
+            plano foi temporariamente desativado.
           </p>
-          <ul className="flex flex-col py-8 gap-4">
+          <p className="text-base pt-4 text-[#A0A0A9]">
+            Mas não se preocupe - nada foi perdido. Seus dados estão salvos, e
+            sua jornada pode continuar exatamente de onde parou.
+          </p>
+          <p className="text-base pt-4 text-[#A0A0A9]">
+            Ao renovar, você recupera:
+          </p>
+          <ul className="flex flex-col py-4 gap-4">
             <li className="flex items-center gap-4">
               <Check size={16} className="text-primary" />
-              <span className="text-base">Curso Modo Caverna</span>
+              <span className="text-base">
+                Todas as funcionalidades do seu plano
+              </span>
             </li>
             <li className="flex items-center gap-4">
               <Check size={16} className="text-primary" />
-              <span className="text-base">Desafio Caverna</span>
+              <span className="text-base">
+                Seu histórico, metas, rotinas e tudo mais
+              </span>
             </li>
             <li className="flex items-center gap-4">
               <Check size={16} className="text-primary" />
-              <span className="text-base">Suporte via chat</span>
+              <span className="text-base">
+                O meu apoio para seguir firme na missão
+              </span>
             </li>
             <p className="text-base pt-4 text-[#A0A0A9]">
-              Gostaria de continuar utilizando todas as ferramentas do sistema?
-              Clique no botão abaixo e assine o Plano Cavernoso com 50% de
-              desconto!
+              Clique abaixo para reativar seu acesso e continuar evoluindo
             </p>
           </ul>
 
@@ -66,7 +75,7 @@ export function UpgradeDialogExpiredPlan({
               href="/settings/plans"
             >
               <Button>
-                Eu quero ser Cavernoso
+                Quero renovar minha assinatura agora
                 <Zap color="#FFF" fill="#fff"></Zap>
               </Button>
             </Link>
@@ -76,7 +85,7 @@ export function UpgradeDialogExpiredPlan({
             >
               <div className=" flex py-2 items-center justify-center rounded">
                 <AlertDialogCancel className="bg-[#121215] text-[#A0A0A9] rounded-lg w-[80%] h-[41px]">
-                  Continuar com o Plano atual
+                  Continuar sem acesso (por enquanto)
                 </AlertDialogCancel>
               </div>
             </Link>
--- src/components/modals/ExpiredPlanPopup.tsx
+++ src/components/modals/ExpiredPlanPopup.tsx
@@ -1,8 +1,10 @@
 import { AlertDialog, AlertDialogContent } from '@/components/ui/alert-dialog'
 import { Button } from '@/components/ui/button'
-import { Check } from 'lucide-react'
+import { AlertDialogCancel } from '@radix-ui/react-alert-dialog'
+import { Check, Zap } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
+import { AlertTitle } from '../ui/alert'
 export function ExpiredPlanPopup({
   open,
   setOpen,
@@ -12,36 +14,77 @@ export function ExpiredPlanPopup({
 }) {
   return (
     <AlertDialog open={open} onOpenChange={setOpen}>
-      <AlertDialogContent className="max-w-[408px] gap-10 border-t-4 border-t-primary sm:rounded-2xl">
-        <Image
-          src={'/images/home/mockup-modo-caverna.png'}
-          width={350}
-          height={195}
-          className="w-full h-auto"
-          alt="Imagens modo caverna"
-        />
-        <h1 className="text-xl font-semibold">Plano Cavernoso Expirado</h1>
-        <ul className="flex flex-col gap-6">
-          <li className="flex items-center gap-4">
-            <Check size={16} className="text-emerald-400" />
-            <span className="text-sm">Acesso a todas as funcionalidades</span>
-          </li>
-          <li className="flex items-center gap-4">
-            <Check size={16} className="text-emerald-400" />
-            <span className="text-sm">Acesso a todas os Cursos</span>
-          </li>
-          <li className="flex items-center gap-4">
-            <Check size={16} className="text-emerald-400" />
-            <span className="text-sm">Suporte Priorizado</span>
-          </li>
-        </ul>
-        <div className="flex flex-col w-full gap-2">
-          <Link
-            className="flex flex-col justify-center w-lg"
-            href="/settings/plans"
-          >
-            <Button>Renovar Agora</Button>
-          </Link>
+      <AlertDialogContent className="flex min-w-[797px] min-h-[590px] gap-10 bg-[#18181B] sm:rounded-2xl">
+        <div className="relative top-4 ">
+          <Image
+            src={'/images/lobo/apontando.png'}
+            width={210}
+            height={364}
+            alt="Imagens modo caverna"
+          />
+        </div>
+        <div className="flex flex-col w-[490px] h-[500px] pt-4">
+          <div className="flex flex-row ">
+            <AlertTitle className="text-2xl font-semibold text-white">
+              Fala Caverna, sinto sua falta por aqui...
+            </AlertTitle>
+          </div>
+          <p className="text-base pt-4 text-[#A0A0A9]">
+            Sua assinatura expirou, e por isso o acesso às ferramentas do seu
+            plano foi temporariamente desativado.
+          </p>
+          <p className="text-base pt-4 text-[#A0A0A9]">
+            Mas não se preocupe - nada foi perdido. Seus dados estão salvos, e
+            sua jornada pode continuar exatamente de onde parou.
+          </p>
+          <p className="text-base pt-4 text-[#A0A0A9]">
+            Ao renovar, você recupera:
+          </p>
+          <ul className="flex flex-col py-4 gap-4">
+            <li className="flex items-center gap-4">
+              <Check size={16} className="text-primary" />
+              <span className="text-base">
+                Todas as funcionalidades do seu plano
+              </span>
+            </li>
+            <li className="flex items-center gap-4">
+              <Check size={16} className="text-primary" />
+              <span className="text-base">
+                Seu histórico, metas, rotinas e tudo mais
+              </span>
+            </li>
+            <li className="flex items-center gap-4">
+              <Check size={16} className="text-primary" />
+              <span className="text-base">
+                O meu apoio para seguir firme na missão
+              </span>
+            </li>
+            <p className="text-base pt-4 text-[#A0A0A9]">
+              Clique abaixo para reativar seu acesso e continuar evoluindo
+            </p>
+          </ul>
+
+          <div className="flex flex-col w-full gap-2">
+            <Link
+              className="flex flex-col justify-center w-lg"
+              href="/settings/plans"
+            >
+              <Button>
+                Quero renovar minha assinatura agora
+                <Zap color="#FFF" fill="#fff"></Zap>
+              </Button>
+            </Link>
+            <Link
+              className="flex flex-col justify-center w-lg"
+              href="/dashboard"
+            >
+              <div className=" flex py-2 items-center justify-center rounded">
+                <AlertDialogCancel className="bg-[#121215] text-[#A0A0A9] rounded-lg w-[80%] h-[41px]">
+                  Continuar sem acesso (por enquanto)
+                </AlertDialogCancel>
+              </div>
+            </Link>
+          </div>
         </div>
       </AlertDialogContent>
     </AlertDialog>
