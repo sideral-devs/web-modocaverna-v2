6ffd2eb3a4209e02800ec217e7c77859505c0b3b
Artur_Linux <nevesdev.ti@gmail.com>
2025-04-24 18:47:05 -0300

    feat: mudando tema do modal da cave store

--- src/app/(protected)/dashboard/dialogs/DialogCaveStore.tsx
+++ src/app/(protected)/dashboard/dialogs/DialogCaveStore.tsx
@@ -17,32 +17,39 @@ export default function DialogCaveStore({
 }) {
   return (
     <Dialog open={isOpen} onOpenChange={setOpen}>
-      <DialogContent className=" border-none h-[590px] !w-[797px] px-20 pt-10 max-w-none bg-gradient-to-b from-[#1C1C1C] to-[#111111] overflow-x-hidden overflow-y-auto scrollbar-minimal">
+      <DialogContent className=" gap-0 border-none h-[590px] !w-[797px] pt-10 max-w-none bg-zinc-200 text-black overflow-x-hidden overflow-y-auto scrollbar-minimal">
         <div
-          className="absolute top-2 right-2 cursor-pointer z-50 bg-zinc-800 p-3 rounded-sm"
+          className="absolute top-2 right-2 cursor-pointer z-50 bg-black p-3 rounded-sm"
           onClick={() => setOpen(false)}
         >
-          <X className="h-4 w-4 cursor-pointer text-red-700 opacity-100" />
+          <X className="h-4 w-4 cursor-pointer text-white opacity-100" />
           <span className="sr-only">Close</span>
         </div>
-        <DialogTitle className="text-2xl ">{poup.title}</DialogTitle>
-        <div className="justify-center flex flex-col items-center gap-10">
-          <div className="w-full h-[354px]  rounded-xl overflow-hidden">
-            <Image
-              src={'/images/dialogs/banner-loja.png'}
-              alt="Banner"
-              className=" object-cover w-full rounded-lg"
-              width={630}
-              height={354}
-            />
-          </div>
+        <DialogTitle className="text-2xl h-[30px] px-20 ">
+          {poup.title}
+        </DialogTitle>
+
+        <div className="w-full h-[389px] flex justify-center  rounded-xl overflow-hidden">
+          <Image
+            src={'/images/dialogs/banner-loja.png'}
+            alt="Banner"
+            className=" w-[693px] h-[389px] max-w-none max-h-none rounded-lg"
+            width={693}
+            height={389}
+          />
         </div>
-        <AlertDialogFooter className=" sm:justify-center flex flex-row sm:flex-row py-5 justify-center gap-20">
+
+        <AlertDialogFooter className="px-20  sm:justify-center flex flex-row sm:flex-row pb-5 justify-center gap-20">
           <h3 className="w-[260px]">
-            Utilize o cupom &quot;<span className='text-[#FFC803]'>APLICATIVO</span>&quot; para garantir um super
-            desconto.
+            Utilize o cupom &quot;
+            <span className="text-primary">APLICATIVO</span>&quot; para garantir
+            um super desconto.
           </h3>
-          <Link className='h-[46px]' href={`https://redirect.lifs.app/sejacaverna`} target="_blank">
+          <Link
+            className="h-[46px]"
+            href={`https://redirect.lifs.app/sejacaverna`}
+            target="_blank"
+          >
             <Button className="w-[214px] h-[46px] text-lg">ACESSAR LOJA</Button>
           </Link>
         </AlertDialogFooter>
