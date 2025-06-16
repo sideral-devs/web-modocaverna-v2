0c670f8a67722d64fd6f7f34cf3cb2340a798794
john0x01 <personal.joaomarcelo.rocha@gmail.com>
2025-06-16 12:48:28 -0300

    chore: Remover card de afiliação desafio caverna

--- src/app/(protected)/members-area/watch/[course]/[module]/[lesson]/page.tsx
+++ src/app/(protected)/members-area/watch/[course]/[module]/[lesson]/page.tsx
@@ -150,7 +150,6 @@ export default function Page({
   }
 
   useEffect(() => {
-    console.log({ animationFinished, showNextButton })
     if (animationFinished && showNextButton) {
       nextLesson()
     }
