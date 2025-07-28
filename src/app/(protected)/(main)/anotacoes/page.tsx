84fc4207f900596a824e62169c937222c4977da1
john0x01 <personal.joaomarcelo.rocha@gmail.com>
2025-07-28 09:22:08 -0300

    styles: Diminuir fonte de emptystate em anotacoes

--- src/app/(protected)/(main)/anotacoes/page.tsx
+++ src/app/(protected)/(main)/anotacoes/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
     <UpgradeModalTrigger>
       <div className="flex flex-col flex-1 items-center justify-center gap-6">
         <StickyNote className="text-primary" size={32} />
-        <h1 className="max-w-lg text-lg md:text-2xl text-center font-semibold">
+        <h1 className="max-w-lg text-lg md:text-xl text-center font-semibold">
           Para visualizar, selecione uma anotação ao lado
         </h1>
       </div>
